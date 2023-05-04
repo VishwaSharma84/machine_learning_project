d2013072e01ea86a9b7d99e8932050e8c772c240
VishwaSharma84 <svishwanath595@gmail.com>
2023-05-04 19:21:47 +0530

    flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting ML project"
+    return "CI/CD established"
 
 if __name__== "__main__":
     app.run(debug=True)
